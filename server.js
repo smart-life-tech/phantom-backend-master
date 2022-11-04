c67568d8c8dfe598ed2c705b71ac1d378f2ab913
Lawrence Ughonu <lawrenceughonu@gmail.com>
2022-11-04 08:11:12 +0100

    make it better

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.use("/api/users", userRoutes)
 //-------------------------------Deployment----------------------------------------------
 
 
-const PORT = process.env.PORT ||  4000;
+const PORT = process.env.PORT ||  5000;
 app.listen(PORT, console.log(`Sever started on port ${PORT}`))
