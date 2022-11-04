17fed43b6fb6a9c68d80ba557af9fcf7203d5a2d
Lawrence Ughonu <lawrenceughonu@gmail.com>
2022-11-04 03:03:58 +0100

    completed backend

--- server.js
+++ server.js
@@ -31,4 +31,4 @@ app.use("/api/users", userRoutes)
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, console.log(`Sever started on port ${PORT}`))
+app.listen(PORT, console.log(`Sever started on port 400`))
